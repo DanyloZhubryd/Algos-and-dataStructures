8e900e90939100b79fee858319e2e5e678cb0bdd
Zhubryd Danylo <zubdan21@gmail.com>
2021-09-22 22:40:00 +0300

    Adding new unit tests

--- src/test/java/ua/lviv/iot/quickSort/QuickSortTests.java
+++ src/test/java/ua/lviv/iot/quickSort/QuickSortTests.java
@@ -18,34 +18,76 @@ import ua.lviv.iot.quickSort.QuickSort;
 @TestInstance(Lifecycle.PER_CLASS)
 public class QuickSortTests {
 	List<Integer> arrayToList;
-	Integer[] sortedArray;
+	LinkedList<Integer> ascSortedList;
+	LinkedList<Integer> descSortedList;
 	LinkedList<Integer> listToSort;
 	
 	@BeforeAll
 	void setUp() {
+		//Not sorted list
 		Integer[] arrayToSort = new Integer[] {1, 6, -4, -8, 13, 5, -2, -12, 15};
 		arrayToList = Arrays.asList(arrayToSort);
 		listToSort = new LinkedList<>(arrayToList);
+		//Ascending sorted list
+		Integer[] ascSortedArray = new Integer[] {-12, -8, -4, -2, 1, 5, 6, 13, 15};
+		arrayToList = Arrays.asList(ascSortedArray);
+		ascSortedList = new LinkedList<Integer>(arrayToList);
+		//Descending sorted list
+		Integer[] descSortedArray = new Integer[] {15, 13, 6, 5, 1, -2, -4, -8, -12};
+		arrayToList = Arrays.asList(descSortedArray);
+		descSortedList = new LinkedList<Integer>(arrayToList);
 	}
 	
 	@Test
 	@DisplayName("Testing ascending sort")
 	void testAscendingSort() {
-		sortedArray = new Integer[] {-12, -8, -4, -2, 1, 5, 6, 13, 15};
-		arrayToList = Arrays.asList(sortedArray);
-		LinkedList<Integer> expectedResult = new LinkedList<Integer>(arrayToList);
-		LinkedList<Integer> actualResult= new LinkedList<Integer>(listToSort);
+		LinkedList<Integer> expectedResult = new LinkedList<>(ascSortedList);
+		LinkedList<Integer> actualResult = new LinkedList<Integer>(listToSort);
 		QuickSort.sorted(actualResult, true);
 		assertEquals(expectedResult, actualResult);
 	}
 	
 	@Test
 	@DisplayName("Testing descending sort")
-	void testDescendingSort() {
-		sortedArray = new Integer[] {15, 13, 6, 5, 1, -2, -4, -8, -12};
-		arrayToList = Arrays.asList(sortedArray);
-		LinkedList<Integer> expectedResult = new LinkedList<Integer>(arrayToList);
-		LinkedList<Integer> actualResult= new LinkedList<Integer>(listToSort);
+	void testDescendingSort() { 
+		LinkedList<Integer> expectedResult = new LinkedList<>(descSortedList);
+		LinkedList<Integer> actualResult = new LinkedList<Integer>(listToSort);
+		QuickSort.sorted(actualResult, false);
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	@DisplayName("Testing ascending sort of ascending sorted list")
+	void testAscSortOfAscSortedList() {
+		LinkedList<Integer> expectedResult = new LinkedList<>(ascSortedList);
+		LinkedList<Integer> actualResult = new LinkedList<>(ascSortedList);
+		QuickSort.sorted(actualResult, true);
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	@DisplayName("Testing descending sort of descending sorted list")
+	void testDescSortOfDescSortedList() {
+		LinkedList<Integer> expectedResult = new LinkedList<>(descSortedList);
+		LinkedList<Integer> actualResult = new LinkedList<>(descSortedList);
+		QuickSort.sorted(actualResult, false);
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	@DisplayName("Testing ascending sort of descending sorted list")
+	void testAscSortOfDescSortedList() {
+		LinkedList<Integer> expectedResult = new LinkedList<>(ascSortedList);
+		LinkedList<Integer> actualResult = new LinkedList<>(descSortedList);
+		QuickSort.sorted(actualResult, true);
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	@DisplayName("Testing descending sort of ascending sorted list")
+	void testDescSortOfAscSortedList() {
+		LinkedList<Integer> expectedResult = new LinkedList<>(descSortedList);
+		LinkedList<Integer> actualResult = new LinkedList<>(ascSortedList);
 		QuickSort.sorted(actualResult, false);
 		assertEquals(expectedResult, actualResult);
 	}
