e3cf1aa0063337381657bbc24261a4d89b882d07
Zhubryd Danylo <zubdan21@gmail.com>
2021-11-29 23:02:35 +0200

    added tests and fixed bug when pattern is in the end of text

--- main.py
+++ main.py
@@ -11,27 +11,28 @@ class RabinKarp:
             hash_result = (self.base * hash_result + ord(str_to_hash[i])) % self.mod
         return hash_result
 
-    def shift_substring(self, substring: int, first_char_transformer: int, first_symbol: int, next_symbol: int):
-        substring = ((substring - first_symbol * first_char_transformer) * self.base + next_symbol) % self.mod
-        if substring < 0:
-            substring = substring + self.mod
-        return substring
+    def shift_substring(self, substring_hash: int, first_char_transformer: int, first_symbol: str, next_symbol: str):
+        substring_hash = ((substring_hash - ord(first_symbol) * first_char_transformer) * self.base
+                          + ord(next_symbol)) % self.mod
+        if substring_hash < 0:
+            substring_hash = substring_hash + self.mod
+        return substring_hash
 
     def create_first_char_transformer(self, pattern_len: int):
         transformer = 1
-        for i in range(pattern_len-1):
+        for i in range(pattern_len - 1):
             transformer = (transformer * self.base) % self.mod
         return transformer
 
-    def find(self, text: str, pattern: str):
+    def find(self, pattern: str, text: str):
         str_len = len(text)
         pattern_len = len(pattern)
         first_char_transformer = self.create_first_char_transformer(pattern_len)
         pattern_hash = self.to_hash(pattern)
         substring_hash = self.to_hash(text[0:pattern_len])
-        result = []
+        found_pos = []
         j = 0
-        for i in range(str_len - pattern_len):
+        for i in range(str_len - pattern_len + 1):
             if pattern_hash == substring_hash:
                 for j in range(pattern_len):
                     if text[i + j] == pattern[j]:
@@ -39,13 +40,14 @@ class RabinKarp:
                     else:
                         break
                 if j == pattern_len:
-                    result.append(i)
-            substring_hash = self.shift_substring(substring_hash, first_char_transformer,
-                                                  ord(text[i]), ord(text[i+pattern_len]))
-        return result
+                    found_pos.append(i)
+            if i != str_len - pattern_len:
+                substring_hash = self.shift_substring(substring_hash, first_char_transformer,
+                                                      text[i], text[i + pattern_len])
+        return found_pos
 
 
 if __name__ == '__main__':
-    rabin_karp = RabinKarp()
-    result = rabin_karp.find("bibabebybobibo", "bybobi")
+    rabin_karp = RabinKarp(256, 1000)
+    result = rabin_karp.find("cab", "abccabbcabaccbaacbabc")
     print(result)
